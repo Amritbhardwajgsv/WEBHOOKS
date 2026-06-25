5934063b6a9a0d992a835ba1cb21b2d990031b4c
Amrit Bhardwaj <amritbharadwaj4@gmail.com>
2026-06-25 12:07:19 +0530

    new-files

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
 } = require('./db');
 
 const app = express();
-const port =  process.env.port || 3000;
+const port =  process.env.port ||process.env.PORT || 3000;
 
 const s3 = new AWS.S3({
     region: process.env.AWS_REGION
